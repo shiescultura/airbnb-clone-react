28c819e91d091c3cd5223de7be829ae8b17cd97c
Shiela  Escultura <shielaescultura@gmail.com>
2022-02-09 02:49:09 +0800

    add feat: Search and search result page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 // import logo from './logo.svg';
-import './App.css';
-import Home from './Home';
-import Header from './Header';
-import Footer from './Footer';
+import './App.css'
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     // BEM
     <div className="App">
-      {/* <h1> let's build airbnb clone</h1> */}
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
 
       {/* Home */}
           {/* Header */}
